762b987ad8fbc02db11f706d7f64ff865ac6e739
Caleb <yocalebo@gmail.com>
2019-04-02 09:31:04 -0400

    add another patch script

---
+++ sh/patch_freenasldap.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+DIFF_FILE="/root/freenasldap.diff"
+ORIG_FILE="/usr/local/www/freenasUI/common/freenasldap.py"
+BACK_FILE="/usr/local/www/freenasUI/common/freenasldap.py.BACK"
+
+verify()
+{
+	if [ -r "/etc/version" ]; then
+		version=$(grep -r 'TrueNAS-11.1-U7' /etc/version 2> /dev/null)
+
+		if [ "$version" = "" ]; then
+			echo "wrong version of TrueNAS"
+			echo "expecting TrueNAS-11.1-U7"
+			echo "exiting"
+			exit 0
+		fi
+
+	else
+		echo "file /etc/version not found"
+		echo "exiting"
+		exit 0
+	fi
+}
+
+backup()
+{
+	if [ -r "${ORIG_FILE}" ]; then
+		echo "backing up original file"
+		if output=$(cp ${ORIG_FILE} ${BACK_FILE} > /dev/null 2>&1); then
+			echo "backup succeeded"
+		else
+			echo "backup failed"
+			echo "exiting"
+			exit 0
+		fi
+	else
+		echo "${ORIG_FILE} not found"
+		echo "exiting"
+		exit 0
+	fi
+}
+
+patch()
+{
+	if [ -r "${DIFF_FILE}" ]; then
+		echo "applying patch"
+		if output=$(/usr/bin/patch ${ORIG_FILE} ${DIFF_FILE} > /dev/null 2>&1); then
+			echo "patch succeeded"
+		else
+			echo "patch failed"
+			echo "restoring original file"
+			if output=$(cp ${BACK_FILE} ${ORIG_FILE} > /dev/null 2>&1); then
+				echo "restored original file"
+				echo "exiting"
+				exit 0
+			else
+				echo "restoring original file failed"
+				echo "please manually intervene"
+				exit 0
+			fi
+		fi
+	else
+		echo "can't find ${DIFF_FILE}"
+		echo "exiting"
+		exit 0
+	fi
+}
+
+restart()
+{
+	echo "restarting necessary services"
+	if output1=$(service middlewared onerestart > /dev/null 2>&1); then
+		echo "restarting middlewared succeeded"
+	else
+		echo "restarting middlewared failed"
+		exit 0
+	fi
+
+	if output2=$(service django onerestart > /dev/null 2>&1); then
+		echo "restarting django succeeded"
+	else
+		echo "restarting django failed"
+		exit 0 
+	fi
+
+	if output3=$(service nginx onerestart > /dev/null 2>&1); then
+		echo "restarting nginx succeeded"
+	else
+		echo "restarting nginx failed"
+		exit 0
+	fi
+}
+
+verify
+backup
+patch
+restart
