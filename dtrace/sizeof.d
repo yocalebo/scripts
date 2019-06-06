89725c998cf899af9c5fc2ddabc6e5f8e128f2b8
Caleb <yocalebo@gmail.com>
2019-06-06 12:40:58 -0400

    fix comment section

--- dtrace/sizeof.d
+++ dtrace/sizeof.d
@@ -1,9 +1,11 @@
 #!/usr/sbin/dtrace -qs
 
-# simple dtrace script to print the size of a struct/union
-# this particular struct is the segment size of a ZFS trim
-# specifically, I used this to see how much RAM it would take
-# by increasing the vfs.zfs.vdev.trim_max_pending parameter
+/*
+ * simple dtrace script to print the size of a struct or a union
+ * this particular struct is the segment size of a ZFS trim
+ * specifically, I used this to see how much RAM it would take
+ * by increasing the vfs.zfs.vdev.trim_max_pending parameter
+ */
 
 BEGIN
 {
